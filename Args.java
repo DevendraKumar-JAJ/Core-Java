f1ca5711e93d7bcc83c66a64ef7614a530266826
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-09-13 06:43:17 +0530

    Args array in java

---
+++ Args.java
@@ -0,0 +1,7 @@
+public class Args {
+  public static void main(String[] args) {
+    int num=-217483649;
+      System.out.println(num);
+      System.out.println(args[0]); // Args inter through java Args "Deeeeeee"
+    }
+}
