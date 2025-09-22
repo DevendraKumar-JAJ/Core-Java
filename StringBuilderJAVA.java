37c06d1d529af9523a02302834a6d4376fc677b6
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-09-22 07:15:23 +0530

    String and StringBuilder in java

---
+++ StringBuilderJAVA.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class StringBuilderJAVA{
+  public static void main(String[] args) {
+    StringBuilder sb=new StringBuilder("Tony");
+    System.out.println(sb);
+  }
+}
---
+++ StringMethod.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class StringMethod{
+  public static void main(String[] args) {
+    // String fname=new String("Devendra ");
+    // String lname=new String("Kumar");
+    // System.out.println(fname+lname);
+    // System.out.println(fname.length());
+    // fname=fname.replace('e','E');
+    // System.out.println(fname);
+    // fname=fname.substring(0,3);
+    // fname=fname.replace('E','e');
+    // System.out.println(fname);
+    // Single word input
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Single word");
+    // String word=sc.next();
+    // System.out.println(word);
+    // System.out.println("Single Line\n");
+    // String line=sc.nextLine();
+    // System.out.println(line);
+
+    // concatenation
+    String w1=sc.next();
+    String w2=sc.next();
+    System.out.println(w1+" "+w2);
+    // String length
+    // System.out.println("Word 1 :"+w1.length());
+    // // Vertical print
+    // for(int i=0;i<w1.length();i++){
+    //   System.out.println(w1.charAt(i));
+    // }
+
+    // compare 
+    // if(w1.compareTo(w2)==0){
+    //   System.out.println("Both string are same");
+    // }else{
+    //   System.out.println("Both are distinct");
+    // }
+    
+  }
+}
