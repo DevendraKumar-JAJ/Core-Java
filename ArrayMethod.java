eb1edeae0596f5d747a7566ae65560f716316e33
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-09-22 07:13:24 +0530

    Array in java

---
+++ ArrayMethod.java
@@ -0,0 +1,12 @@
+class ArrayMethod{
+  public static void main(String args[]){
+    System.out.println("Array in JAVA.");
+    int marks[]={30,40,50};
+    int [] num={1,2,3,4};
+    System.out.println("Eng: "+marks[0]);
+    int[] subs=new int[3];
+    subs[0]=1;
+    subs[1]=2;
+    System.out.println(subs.length);
+  }
+}
