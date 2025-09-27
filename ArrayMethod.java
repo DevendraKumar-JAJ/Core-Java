16460aa5183d91e7cf3c89878487cbf246dda2fa
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-09-27 12:22:34 +0530

    Array in java

--- ArrayMethod.java
+++ ArrayMethod.java
@@ -1,12 +1,18 @@
+import java.util.*;
 class ArrayMethod{
   public static void main(String args[]){
-    System.out.println("Array in JAVA.");
-    int marks[]={30,40,50};
-    int [] num={1,2,3,4};
-    System.out.println("Eng: "+marks[0]);
-    int[] subs=new int[3];
-    subs[0]=1;
-    subs[1]=2;
-    System.out.println(subs.length);
+    // System.out.println("Array in JAVA.");
+    // int marks[]={30,40,50};
+    // int [] num={1,2,3,4};
+    // System.out.println("Eng: "+marks[0]);
+    // int[] subs=new int[3];
+    // subs[0]=1;
+    // subs[1]=2;
+    // System.out.println(subs.length);
+    
+   int[] intArray = {1, 5, 8};
+for (int ele : intArray) {
+    System.out.println(ele);
+}
   }
 }
